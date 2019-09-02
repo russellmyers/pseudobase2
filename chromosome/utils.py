45e81696dd350834890fd3613ab47e8c89529a88
russellmyers <russell.myers@ngahr.com>
2019-09-02 23:19:07 +1000

    v2.05e3 - recomment out import pandas
    v2.05e3 - recomment out import pandas - seems to be a problem when
    running chromosome_batch_import on a2hosted server

--- chromosome/utils.py
+++ chromosome/utils.py
@@ -3,7 +3,7 @@ from __future__ import print_function # cater for printing on same line, ie with
 #import matplotlib.pyplot as plt
 #import numpy as np
 import gzip
-import pandas as pd
+#import pandas as pd
 
 '''
  VCF Analysis and Conversion routines 
